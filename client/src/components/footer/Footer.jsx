3e8b5bfb9e10580ffa6770ca2a2d585d097d5bc4
VeronikaManev <157894969+VeronikaManev@users.noreply.github.com>
2024-05-29 11:31:54 +0200

    Add footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,25 @@
+import './Footer.css';
+
+export function Footer(params) {
+    return (
+        <footer className='footer'>
+            <div className='container'>
+
+                <ul className="footer-links">
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+
+                <p>&copy; 2024 CampCompass. All Rights Reserved.</p>
+
+                <ul className="social-media">
+                    <li><a href="https://www.facebook.com"><i className="fab fa-facebook-f"></i></a></li>
+                    <li><a href="https://www.instagram.com"><i className="fab fa-instagram"></i></a></li>
+                    <li><a href="https://www.tweeter.com"><i className="fab fa-twitter"></i></a></li>
+                    <li><a href="https://www.youtube.com"><i className="fab fa-youtube"></i></a></li>
+                </ul>
+
+            </div>
+        </footer>
+    )
+}
