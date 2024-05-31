54119129a3604b9350233bc589af45cc1bfd59fd
VeronikaManev <157894969+VeronikaManev@users.noreply.github.com>
2024-05-31 10:44:24 +0200

    Add catalog view

---
+++ client/src/components/catalog/Catalog.jsx
@@ -0,0 +1,193 @@
+import './Catalog.css';
+import heroImg from './image/pexels-alan-caldwell-185375-587976cut.jpg';
+
+
+export function Catalog(params) {
+    return (
+        <div className='catalogContainer'>
+            <section className="imgContainer">
+                <img className='heroImg' src={heroImg} alt="image-example" />
+                <h2 className='title'>Find your dream vacation now</h2>
+                <p className='subtitle'>Navigate Your Next Adventure</p>
+            </section>
+
+            <section className='catalog'>
+
+                <div className="catalogItem">
+
+                    <div className="itemImgContainer">
+                        <img className="itemImg" src="https://kamperen.qodeinteractive.com/wp-content/uploads/2021/06/Single-feat-img-10.jpg" alt="" />
+                    </div>
+
+                    <div className="itemInfoContainer">
+                        <h3>Lorem, ipsum dolor</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In error rerum ipsum nihil cum porro omnis tempora nobis iste sequi.</p>
+                    </div>
+
+                    <div className="itemMoreContainer">
+
+                        <div className="facilitiesContainer">
+                            <i className="fas fa-umbrella-beach"></i>
+                            <i className="fas fa-paw"></i>
+                            <i className="fas fa-shower"></i>
+                            <i className="fas fa-wifi"></i>
+                        </div>
+
+                        <div className="moreInfo">
+                            <span>More</span>
+                            <button className="moreBtn"><i class="fas fa-arrow-right"></i></button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="catalogItem">
+
+                    <div className="itemImgContainer">
+                        <img className="itemImg" src="https://kamperen.qodeinteractive.com/wp-content/uploads/2021/06/Single-feat-img-10.jpg" alt="" />
+                    </div>
+
+                    <div className="itemInfoContainer">
+                        <h3>Lorem, ipsum dolor</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In error rerum ipsum nihil cum porro omnis tempora nobis iste sequi.</p>
+                    </div>
+
+                    <div className="itemMoreContainer">
+
+                        <div className="facilitiesContainer">
+                            <i className="fas fa-umbrella-beach"></i>
+                            <i className="fas fa-paw"></i>
+                            <i className="fas fa-shower"></i>
+                            <i className="fas fa-wifi"></i>
+                        </div>
+
+                        <div className="moreInfo">
+                            <span>More</span>
+                            <button className="moreBtn"><i className="fas fa-arrow-right"></i></button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="catalogItem">
+
+                    <div className="itemImgContainer">
+                        <img className="itemImg" src="https://kamperen.qodeinteractive.com/wp-content/uploads/2021/06/Single-feat-img-10.jpg" alt="" />
+                    </div>
+
+                    <div className="itemInfoContainer">
+                        <h3>Lorem, ipsum dolor</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In error rerum ipsum nihil cum porro omnis tempora nobis iste sequi.</p>
+                    </div>
+
+                    <div className="itemMoreContainer">
+
+                        <div className="facilitiesContainer">
+                            <i className="fas fa-umbrella-beach"></i>
+                            <i className="fas fa-paw"></i>
+                            <i className="fas fa-shower"></i>
+                            <i className="fas fa-wifi"></i>
+                        </div>
+
+                        <div className="moreInfo">
+                            <span>More</span>
+                            <button className="moreBtn"><i className="fas fa-arrow-right"></i></button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="catalogItem">
+
+                    <div className="itemImgContainer">
+                        <img className="itemImg" src="https://kamperen.qodeinteractive.com/wp-content/uploads/2021/06/Single-feat-img-10.jpg" alt="" />
+                    </div>
+
+                    <div className="itemInfoContainer">
+                        <h3>Lorem, ipsum dolor</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In error rerum ipsum nihil cum porro omnis tempora nobis iste sequi.</p>
+                    </div>
+
+                    <div className="itemMoreContainer">
+
+                        <div className="facilitiesContainer">
+                            <i className="fas fa-umbrella-beach"></i>
+                            <i className="fas fa-paw"></i>
+                            <i className="fas fa-shower"></i>
+                            <i className="fas fa-wifi"></i>
+                        </div>
+
+                        <div className="moreInfo">
+                            <span>More</span>
+                            <button className="moreBtn"><i className="fas fa-arrow-right"></i></button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="catalogItem">
+
+                    <div className="itemImgContainer">
+                        <img className="itemImg" src="https://kamperen.qodeinteractive.com/wp-content/uploads/2021/06/Single-feat-img-10.jpg" alt="" />
+                    </div>
+
+                    <div className="itemInfoContainer">
+                        <h3>Lorem, ipsum dolor</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In error rerum ipsum nihil cum porro omnis tempora nobis iste sequi.</p>
+                    </div>
+
+                    <div className="itemMoreContainer">
+
+                        <div className="facilitiesContainer">
+                            <i className="fas fa-umbrella-beach"></i>
+                            <i className="fas fa-paw"></i>
+                            <i className="fas fa-shower"></i>
+                            <i className="fas fa-wifi"></i>
+                        </div>
+
+                        <div className="moreInfo">
+                            <span>More</span>
+                            <button className="moreBtn"><i className="fas fa-arrow-right"></i></button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="catalogItem">
+
+                    <div className="itemImgContainer">
+                        <img className="itemImg" src="https://kamperen.qodeinteractive.com/wp-content/uploads/2021/06/Single-feat-img-10.jpg" alt="" />
+                    </div>
+
+                    <div className="itemInfoContainer">
+                        <h3>Lorem, ipsum dolor</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In error rerum ipsum nihil cum porro omnis tempora nobis iste sequi.</p>
+                    </div>
+
+                    <div className="itemMoreContainer">
+
+                        <div className="facilitiesContainer">
+                            <i className="fas fa-umbrella-beach"></i>
+                            <i className="fas fa-paw"></i>
+                            <i className="fas fa-shower"></i>
+                            <i className="fas fa-wifi"></i>
+                        </div>
+
+                        <div className="moreInfo">
+                            <span>More</span>
+                            <button className="moreBtn"><i className="fas fa-arrow-right"></i></button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </section>
+        </div>
+    )
+}
