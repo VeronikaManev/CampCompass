45fc756aa34df3fc7cb245ad73dfeb661010f442
VeronikaManev <157894969+VeronikaManev@users.noreply.github.com>
2024-06-11 12:01:28 +0200

    Add some changes in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import { Login } from './components/login/Login';
 import { Register } from './components/register/Register';
 import { Catalog } from './components/catalog/Catalog';
 import { Best } from './components/best/Best';
+import { Campsite } from './components/camp/Campsite';
 
 
 
@@ -24,7 +25,13 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/catalog' element={<Catalog />} />
-        <Route path='/best' element={<Best />} />
+
+
+
+        <Route path='/catalog/3564027f-adcd-4425-b2c0-1253d2386c0c' element={<Campsite />} /> /
+      
+
+        
 
       </Routes>
       <Footer></Footer>
