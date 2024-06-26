4de043f112560db062b68934f80742a0299d32a3
VeronikaManev <157894969+VeronikaManev@users.noreply.github.com>
2024-06-26 11:52:53 +0200

    Add some changes in login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -6,13 +6,14 @@ import heroImg from './image/pexels-cliford-mervil-988071-2398220.jpg';
 
 export function Login(params) {
     const navigate = useNavigate();
-const {values, changeHandler} = useForm({email:'', password:''});
+    
+    const { values, changeHandler } = useForm({ email: '', password: '' });
 
-async function onSubmit(e) {
-    e.preventDefault();
-    await login(values.email, values.password);
-    navigate('/');
-}
+    async function onSubmit(e) {
+        e.preventDefault();
+        await login(values.email, values.password);
+        navigate('/');
+    }
     return (
         <div className='loginContainer'>
             <img className='heroImg' src={heroImg} alt="image-example" />
@@ -24,7 +25,7 @@ async function onSubmit(e) {
                     <input onChange={changeHandler} type="email" name='email' placeholder='E-mail' value={values.email} />
 
                     <label htmlFor="password"><i className="fas fa-lock"></i></label>
-                    <input onChange={changeHandler} type="password" name='password' placeholder='Password' value={values.password}/>
+                    <input onChange={changeHandler} type="password" name='password' placeholder='Password' value={values.password} />
 
                     <button className="loginBtn"><i className="fas fa-arrow-right"></i></button>
                 </form>
