d61b217f0c29ae3936452240d334b292b64fff63
Veronika Doychinova <veronicadoychinova@gmail.com>
2024-07-21 15:07:20 +0200

    Add some changes in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css';
+
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+import { login } from '../data/auth';
+
 import { Home } from './components/home/Home';
 import { About } from './components/about/About';
 import { Header } from './components/header/Header';
@@ -9,15 +14,21 @@ import { Register } from './components/register/Register';
 import { Catalog } from './components/catalog/Catalog';
 import { Campsite } from './components/campsite/Campsite';
 import { Create } from './components/create/Create';
-
-
+import { ContextProvider } from './context/context';
 
 
 function App() {
 
+  // const [auth, setAuth] = useState({});
+
+  // const loginSubmitHandler = async (values) => {
+  //   const user = await login(values.email, values.password);
+  //   setAuth(user);
+  // }
+
   return (
 
-    <>
+    <ContextProvider>
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home />} />
@@ -31,7 +42,7 @@ function App() {
         <Route path='/details/:id' element={<Campsite />} /> /
       </Routes>
       <Footer></Footer>
-    </>
+    </ContextProvider>
 
   )
 }
