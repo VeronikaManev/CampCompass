6f153f10750ee0232d442508e133c7377a6019aa
Veronika Doychinova <veronicadoychinova@gmail.com>
2024-07-21 15:07:03 +0200

    Add some changes in header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,23 @@
+import { useContext } from 'react';
 import './Header.css';
 import logoImg from './image/logo-transparent-png.png';
 import { Link } from 'react-router-dom';
+import { Context } from '../../context/context';
+
 
 export function Header(params) {
+    const { contextData } = useContext(Context);
+
     return (
         <header className="header">
             <Link to='/'><img className='logoImg' src={logoImg} alt="logo" /></Link>
             <nav className="navigation">
                 <ul className="list">
                     <li> <Link to='/about' className="list-item">About</Link> </li>
-                    <li> <Link to='/login' className="list-item">Login</Link> </li>
-                    <li> <Link to='/register' className="list-item">Register</Link> </li>
+                    {!contextData.userData && <li> <Link to='/login' className="list-item">Login</Link> </li>}
+                    {!contextData.userData && <li> <Link to='/register' className="list-item">Register</Link> </li>}
                     <li> <Link to='/catalog' className="list-item">Catalog</Link> </li>
-                    <li> <Link to='/create' className="list-item">Create</Link> </li>
+                    {contextData.userData && <li> <Link to='/create' className="list-item">Create</Link> </li>}
 
                 </ul>
             </nav>
