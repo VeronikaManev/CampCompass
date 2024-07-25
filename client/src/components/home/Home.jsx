1e6b9e15746a254c4d6005aaa8ceff87112bcefb
Veronika Doychinova <veronicadoychinova@gmail.com>
2024-07-25 12:23:24 +0200

    Add some desing changes in home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,9 @@
 import './Home.css';
-import heroImg from './image/TCS-Camping-Sempach----Luftaufnahme-des-Campingplatzes-am-Wasser-2e2bc516-67d0-463d-82e7-48a22fcb0c6b.jpg';
 
 
 export function Home(params) {
     return (
         <div className='homeContainer'>
-            <img className='heroImg' src={heroImg} alt="image-example" />
             <h1 className='title'>CampCompass</h1>
             <p className='subtitle'>Navigate Your Next Adventure</p>
 
