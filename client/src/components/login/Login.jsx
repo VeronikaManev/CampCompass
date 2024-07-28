56eeb2a473d7876ff29f8ca07ec969e46d46179b
Veronika Doychinova <veronicadoychinova@gmail.com>
2024-07-28 12:13:59 +0200

    Add design changes im Login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -4,6 +4,8 @@ import { useForm } from '../../hooks/useForm';
 import './Login.css';
 import { useContext } from 'react';
 import { Context } from '../../context/Context';
+import heroImg from './image/NOpexels-cliford-mervil-988071-2398220.jpg';
+
 
 
 export function Login() {
@@ -26,25 +28,29 @@ export function Login() {
     }
 
     return (
-        <div className='loginContainer'>
-
-            <div className='container'>
-
-                <form onSubmit={onSubmit} className='form' action="">
-                    <label htmlFor="emial"><i className="fas fa-user"></i></label>
-                    <input onChange={changeHandler} type="email" name='email' placeholder='E-mail' value={values.email} />
-
-                    <label htmlFor="password"><i className="fas fa-lock"></i></label>
-                    <input onChange={changeHandler} type="password" name='password' placeholder='Password' value={values.password} />
-
-                    <button className="loginBtn"><i className="fas fa-arrow-right"></i></button>
-                </form>
-
-                <div className='newUserContainer'>
-                    <p>New User?</p>
-                    <Link to='/register' className="goToRegister">Create an Account</Link>
-                </div>
+        <>
+            <div className="backgroundContainerLogin">
+                <img src={heroImg} alt="" />
             </div>
-        </div>
+
+            <div className='loginContainer'>
+
+
+                    <form onSubmit={onSubmit} className='form' action="">
+                        <label htmlFor="emial"><i className="fas fa-user"></i></label>
+                        <input onChange={changeHandler} type="email" name='email' placeholder='E-mail' value={values.email} />
+
+                        <label htmlFor="password"><i className="fas fa-lock"></i></label>
+                        <input onChange={changeHandler} type="password" name='password' placeholder='Password' value={values.password} />
+
+                        <button className="loginBtn"><i className="fas fa-arrow-right"></i></button>
+                    </form>
+
+                    <div className='newUserContainer'>
+                        <p>New User?</p>
+                        <Link to='/register' className="goToRegister">Create an Account</Link>
+                    </div>
+            </div>
+        </>
     )
 }
