e88190140fcac5c86d8b0ca690ba7f82f98078c4
Veronika Doychinova <veronicadoychinova@gmail.com>
2024-07-28 12:05:08 +0200

    Adds design changes

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,18 @@
 import './Home.css';
+import heroImg from './image/NOTCS-Camping-Sempach----Luftaufnahme-des-Campingplatzes-am-Wasser-2e2bc516-67d0-463d-82e7-48a22fcb0c6b.jpg';
+
 
 
 export function Home(params) {
     return (
-        <div className='homeContainer'>
-            <h1 className='title'>CampCompass</h1>
-            <p className='subtitle'>Navigate Your Next Adventure</p>
-
-        </div>
+        <>
+            <div className="backgroundContainerHome">
+                <img src={heroImg} alt="" />
+            </div>
+            <div className='homeContainer'>
+                <h1 className='title'>Camp Compass</h1>
+                <p className='subtitle'>Navigate Your Next Adventure</p>
+            </div>
+        </>
     )
 }
