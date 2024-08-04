c52becd7932834402c47d1eb31bc35fc7851fba7
Veronika Doychinova <veronicadoychinova@gmail.com>
2024-08-04 14:32:19 +0200

    Add target blank for all links

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -13,10 +13,10 @@ export function Footer() {
                 <p>&copy; 2024 CampCompass. All Rights Reserved.</p>
 
                 <ul className="social-media">
-                    <li><a href="https://www.facebook.com"><i className="fab fa-facebook-f"></i></a></li>
-                    <li><a href="https://www.instagram.com"><i className="fab fa-instagram"></i></a></li>
-                    <li><a href="https://www.tweeter.com"><i className="fab fa-twitter"></i></a></li>
-                    <li><a href="https://www.youtube.com"><i className="fab fa-youtube"></i></a></li>
+                    <li><a href="https://www.facebook.com" target='_blank'><i className="fab fa-facebook-f"></i></a></li>
+                    <li><a href="https://www.instagram.com" target='_blank'><i className="fab fa-instagram"></i></a></li>
+                    <li><a href="https://x.com/tweeter" target='_blank'><i className="fab fa-twitter"></i></a></li>
+                    <li><a href="https://www.youtube.com" target='_blank'><i className="fab fa-youtube"></i></a></li>
                 </ul>
 
             </div>
