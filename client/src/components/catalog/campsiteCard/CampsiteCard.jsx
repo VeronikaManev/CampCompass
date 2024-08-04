8d69b4f9b801d11dcb77c74536fc5098ef79cc87
Veronika Doychinova <veronicadoychinova@gmail.com>
2024-08-04 14:52:20 +0200

    Add desing changes

--- client/src/components/catalog/campsiteCard/CampsiteCard.jsx
+++ client/src/components/catalog/campsiteCard/CampsiteCard.jsx
@@ -9,35 +9,36 @@ export function CampsiteCard(props) {
     const campsite = props.data;
 
     return (
-        <div className="catalogItem">
+        <Link to={`/details/${campsite._id}`}>
 
-            <div className="itemImgContainer">
-                <img className="itemImg" src={campsite.imageUrl} alt="example" />
-            </div>
+            <div className="catalogItem">
 
-            <div className="itemInfoContainer">
-                <h3>{campsite.title}</h3>
-                <p>{campsite.info}</p>
-            </div>
-
-            <div className="itemMoreContainer">
-
-                <div className="facilitiesContainer">
-                    {campsite.beach && <li> <i className="fas fa-umbrella-beach"></i></li>}
-                    {campsite.petFriendly && <li><i className="fas fa-paw"></i></li>}
-                    {campsite.shower && <li><i className="fas fa-shower"></i></li>}
-                    {campsite.wifi && <li> <i className="fas fa-wifi"></i></li>}
+                <div className="itemImgContainer">
+                    <img className="itemImg" src={campsite.imageUrl} alt="example" />
                 </div>
 
-                <div className="moreInfo">
-                    <Link to={`/details/${campsite._id}`}>
+                <div className="itemInfoContainer">
+                    <h3>{campsite.title}</h3>
+                    <p>{campsite.info}</p>
+                </div>
+
+                <div className="itemMoreContainer">
+
+                    <div className="facilitiesContainer">
+                        {campsite.beach && <li> <i className="fas fa-umbrella-beach"></i></li>}
+                        {campsite.petFriendly && <li><i className="fas fa-paw"></i></li>}
+                        {campsite.shower && <li><i className="fas fa-shower"></i></li>}
+                        {campsite.wifi && <li> <i className="fas fa-wifi"></i></li>}
+                    </div>
+
+                    <div className="moreInfo">
+
                         <span>More</span>
                         <button className="moreBtn"><i className="fas fa-arrow-right"></i></button>
-                    </Link>
+                    </div>
+
                 </div>
-
-            </div>
-
-        </div>
+            </div >
+        </Link >
     )
 }
